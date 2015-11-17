2443bea73b3a919af226708b80b4b39c41b1a6da
András Fincza <drawain@gmail.com>
2015-11-17 09:37:54 +0100

    Update string-calculator.js

--- completed-practices/1-string-calculator/string-calculator.js
+++ completed-practices/1-string-calculator/string-calculator.js
@@ -8,14 +8,14 @@ StringCalculator.prototype = {
   sum: function(numberAsString) {
     if (!numberAsString) return 0;
     var numbers = this._getNumbersFromString(numberAsString);
-    return this._addNumbers(numbers);
+    return this._sum(numbers);
   },
 
   _getNumbersFromString: function(numberAsString) {
     return numberAsString.split(/[\n,]/).map(parseFloat);
   },
 
-  _addNumbers: function(numbers) {
+  _sum: function(numbers) {
     return numbers.reduce(this._add, 0);
   },
 
